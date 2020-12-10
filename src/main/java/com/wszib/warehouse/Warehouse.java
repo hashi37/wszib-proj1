f5fba5f792d2c7e22d7e2da9f7fad0f8f7bcfd4b
hanak <jakubkanah@gmail.com>
2020-12-10 20:45:56 +0100

    Switch case bedzie lepszy

--- src/main/java/com/wszib/warehouse/Warehouse.java
+++ src/main/java/com/wszib/warehouse/Warehouse.java
@@ -10,11 +10,17 @@ import java.util.Scanner;
 @Component("warehouse")
 public class Warehouse {
 
+    final static String COMMAND_HELP = "help";
+    final static String COMMAND_ADD = "add";
+    final static String COMMAND_REMOVE = "remove";
+    final static String COMMAND_LIST = "list";
+    final static String COMMAND_EXIT = "exit";
+
     private int run = 1;
     private int currentItemId = 1;
     private WarehouseItem item;
     //mozliwe komendy w kolekcji List
-    private List<String> commands = Arrays.asList("help", "add", "remove", "list", "exit");
+    private List<String> commands = Arrays.asList(COMMAND_HELP, COMMAND_ADD, COMMAND_REMOVE, COMMAND_LIST, COMMAND_EXIT);
 
     //nasza mini baza danych w kolekcji typu ArrayList
     private List<WarehouseItem> productsInWarehouse = new ArrayList<>();
@@ -46,7 +52,7 @@ public class Warehouse {
                 String[] inputParameters = inputString.split(" ", 2);
 
                 //komenda
-                String command = inputParameters[0];
+                String command = inputParameters[0].toLowerCase();
 
                 //argument
                 String argument = "";
@@ -56,41 +62,51 @@ public class Warehouse {
 
                 //sprawdzamy czy znamy komende
                 if (commands.contains(command)) {
-                    if (command.equalsIgnoreCase("exit")) {
-                        // jak komenda exit to ustaw flage run na 0 i wyjdz
-                        run = 0;
-                    } else if (command.equalsIgnoreCase("help")) {
-                        // wypisz pomoc
-                        System.out.println("Dostepne komendy: help, add, remove, list, exit");
-                        System.out.println("help - wyswietla pomoc, add NowyProdukt - dodaje, remove - StaryProdukt - usuwa, list - wyswietla co jest w magazynie, exit - zamyka program");
-                    } else if (command.equalsIgnoreCase("add")) {
-                        // dodaj element (przechowywany w zmiennej argument) to kolekcji ArrayList nazwanej productsInwarehouse
-                        System.out.println("Dodaje: " + argument);
-                        item = new WarehouseItem();
-                        item.setName(argument);
-                        item.setId(currentItemId);
-                        productsInWarehouse.add(item);
-                        currentItemId++;
-                    } else if (command.equalsIgnoreCase("remove")) {
-                        // usun element z kolekcji ArrayList nazwanej productsInwarehouse
-                        System.out.println("Usuwam element o id: " + argument);
-                        WarehouseItem itemToRemove = findInProductsInWarehouseList(Integer.parseInt(argument));
-                        if(itemToRemove == null){
-                            System.out.println("Nie ma elementu o id: " + argument);
-                            return;
-                        }
-                        productsInWarehouse.remove(itemToRemove);
-                    } else if (command.equalsIgnoreCase("list")) {
-                        // wypisz co mamy zapisane w kolekcji ArrayList productsInwarehouse
-                        System.out.println("Produkty w bazie:");
-                        System.out.println(productsInWarehouse.toString());
-                    } else {
-                        // nie powinnismy tu nigdy trafic!
-                        System.out.println("?!");
+
+                    switch (command) {
+                        case COMMAND_EXIT:
+                            // jak komenda exit to ustaw flage run na 0 i wyjdz
+                            run = 0;
+                            break;
+                        case COMMAND_HELP:
+                            // wypisz pomoc
+                            System.out.println("Dostepne komendy: help, add, remove, list, exit");
+                            System.out.println("help - wyswietla pomoc, add NowyProdukt - dodaje, remove - StaryProdukt - usuwa, list - wyswietla co jest w magazynie, exit - zamyka program");
+                            break;
+                        case COMMAND_ADD:
+                            // dodaj element (przechowywany w zmiennej argument) to kolekcji ArrayList nazwanej productsInwarehouse
+                            System.out.println("Dodaje: " + argument);
+                            item = new WarehouseItem();
+                            item.setName(argument);
+                            item.setId(currentItemId);
+                            productsInWarehouse.add(item);
+                            currentItemId++;
+                            break;
+                        case COMMAND_REMOVE:
+                            // usun element z kolekcji ArrayList nazwanej productsInwarehouse
+                            System.out.println("Usuwam element o id: " + argument);
+                            WarehouseItem itemToRemove = findInProductsInWarehouseList(Integer.parseInt(argument));
+                            if (itemToRemove == null) {
+                                System.out.println("Nie ma elementu o id: " + argument);
+                                return;
+                            }
+                            productsInWarehouse.remove(itemToRemove);
+                            break;
+                        case COMMAND_LIST:
+                            // wypisz co mamy zapisane w kolekcji ArrayList productsInwarehouse
+                            System.out.println("Produkty w bazie:");
+                            System.out.println(productsInWarehouse.toString());
+                            break;
+                        default:
+                            // nie powinnismy tu nigdy trafic!
+                            System.out.println("?!");
+                            break;
                     }
+
                 } else {
                     System.out.println("Nieznana komenda!");
                 }
+
             }
 
         }
